8cf88ff9fe2a7e0708217798f112f1e1524cde57
Eng.Mahammed BD <mamb121@hotmail.com>
2024-10-29 01:43:51 +0300

    Create TopBar And Sidebar

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -1,15 +1,44 @@
-import {ColorModeContext, useMode } from './theme';
-import {CssBaseline, ThemeProvider } from '@mui/material';
-import {Topbar} from './scenes/global/Topbar';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import { Routes, Route } from "react-router-dom"
+import Topbar from './scenes/global/Topbar';
+import Sidebar from './scenes/global/Sidebar';
+import Dashboard from './scenes/dashboard';
+// import Team from './scenes/team';
+// import Invoices from './scenes/invoices';
+// import Contacts from './scenes/contacts';
+// import Bar from './scenes/bar';
+// import Form from './scenes/form';
+// import Line from './scenes/line';
+// import Pie from './scenes/pie';
+// import FAQ from './scenes/faq';
+// import Gography from './scenes/gography';
+// import Calender from './scenes/calender';
+
+
+
 function App() {
   const [theme,colorMode] = useMode();
   return (<ColorModeContext.Provider value={colorMode}>
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <div className="app">
-        <main className='content'>
-          <Topbar/>
-        </main>    
+        <main className="content">
+          <Topbar />
+          <Routes>
+           <Route path="/" element={<Dashboard />} />
+           {/* <Route path="/team" element={<Team />} />
+           <Route path="/invoices" element={<Invoices />} />
+           <Route path="/contacts" element={<Contacts />} />
+           <Route path="/bar" element={<Bar />} />
+           <Route path="/form" element={<Form />} />
+           <Route path="/line" element={<Line />} />
+           <Route path="/pie" element={<Pie />} />
+           <Route path="/faq" element={<FAQ />} />
+           <Route path="/gography" element={<Gography />} />
+           <Route path="/calender" element={<Calender />} /> */}
+          </Routes>
+        </main>
       </div>;      
     </ThemeProvider>    
   </ColorModeContext.Provider>
